baa24889d1dbcff466bb9fc10cf4f6e054a871a6
kelvin364 <k.rwihimba@alustudent.com>
2025-01-09 15:24:33 +0200

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -2,4 +2,19 @@ import uuid
 import datetime
 
 class BaseModel:
-    pass
+    '''BaseModel class'''
+    def __init__(self, **kwargs):
+        '''BaseModel class constructor'''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"[<class name>] (<self.id>) <self.__dict__>"
