c2738be8f2921297697b28b6233857ad3327274b
kelvin364 <k.rwihimba@alustudent.com>
2025-01-09 15:26:19 +0200

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -17,4 +17,4 @@ class BaseModel:
             self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return f"[<class name>] (<self.id>) <self.__dict__>"
+         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
