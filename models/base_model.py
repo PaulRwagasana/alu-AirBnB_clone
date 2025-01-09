e8027c7d1b0af712a43138e3cf9b35195669c88c
kelvin364 <k.rwihimba@alustudent.com>
2025-01-09 15:41:58 +0200

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -18,3 +18,16 @@ class BaseModel:
 
     def __str__(self):
          return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        '''updates the public instance attribute updated_at with the current datetime'''
+        self.updated_at = datetime.datetime.now()
+    
+    def to_dict(self):
+        '''returns a dictionary containing all keys/values of __dict__ of the instance'''
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+    
