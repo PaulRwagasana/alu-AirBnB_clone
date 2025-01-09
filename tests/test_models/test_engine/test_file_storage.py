da6383d60708dbd68617c5993a30c681463e700c
kelvin364 <k.rwihimba@alustudent.com>
2025-01-09 14:43:22 +0200

    commit

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+from models.engine.file_storage import FileStorage
+import unittest
+
+'''Test file storage class'''
+
+class TestFileStorage(unittest.TestCase):
+    '''Test the file storage'''
+    def test_FileStorage(self):
+        '''Test the User file storage'''
+        pass
