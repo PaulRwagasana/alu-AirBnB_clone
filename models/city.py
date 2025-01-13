a11817b139614fddcee3a03516cc41ab02d112a9
PaulRwagasana <p.rwagasana@alustudent.com>
2025-01-13 16:56:23 +0200

    commit

--- models/city.py
+++ models/city.py
@@ -4,4 +4,14 @@ from models.base_model import BaseModel
 '''City class'''
 class City(BaseModel):
     '''City class that inherits from BaseModel'''
+      Attributes:
+        state_id (str): The state id
+        name (str): The name of the city
+    """
+    state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = City.name
+        # self.state_id = City.state_id
