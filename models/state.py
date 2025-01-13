7e31c0969318c6ea1024c021af47b1518b6000cc
PaulRwagasana <p.rwagasana@alustudent.com>
2025-01-13 16:58:52 +0200

    commit

--- models/state.py
+++ models/state.py
@@ -5,3 +5,14 @@ from models.base_model import BaseModel
 class State(BaseModel):
     '''State class that inherits from BaseModel'''
     pass
+        """
+    This is the state class
+
+    Attributes:
+        name (str): The name of the state
+    """
+    name = ""
+
+def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = State.name
