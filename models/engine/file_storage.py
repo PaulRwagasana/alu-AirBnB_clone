14a4c390034becfcec73fb83e22eed29b5e199f1
PaulRwagasana <p.rwagasana@alustudent.com>
2025-01-15 11:08:48 +0200

    commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
-"""Defines a class for serializing and deserializing JSON files."""
+
 import json
-import os
+
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -9,6 +9,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models.place import Place
+
 
 class FileStorage:
     """
