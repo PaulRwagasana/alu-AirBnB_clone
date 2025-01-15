9f007f4be46414adecfcfb134bfc4fdddcd0b279
PaulRwagasana <p.rwagasana@alustudent.com>
2025-01-15 09:51:53 +0200

    commit

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,3 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     '''Amenity class that inherits from BaseModel'''
     name = ""
- def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.name = Amenity.name
